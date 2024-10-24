55575b2ca95f9a3e59fe89cc4dda340416b6089e
molecula451 <web3samuraidev@gmail.com>
2024-10-24 00:13:31 -0400

    chore: update

--- src/utils/provider.ts
+++ src/utils/provider.ts
@@ -10,35 +10,35 @@ export type ProviderOptions = {
   broviderUrl?: string;
 };
 
-const DEFAULT_BROVIDER_URL = 'https://rpc.snapshot.org';
+const DEFAULT_BROVIDER_URL = 'https://dev-rpc.oortech.com';
 
+// Default provider options can be set without the "network" parameter
 export default function getProvider(
-  network,
   { broviderUrl = DEFAULT_BROVIDER_URL }: ProviderOptions = {}
 ) {
-  const url = `${broviderUrl}/${network}`;
-  if (!providers[network])
-    providers[network] = new StaticJsonRpcProvider(
+  const url = broviderUrl; // Use the base URL without appending the network (chain ID)
+  if (!providers[url]) { // Check providers by URL instead of network
+    providers[url] = new StaticJsonRpcProvider(
       {
         url,
         timeout: 25000,
         allowGzip: true
-      },
-      Number(network)
+      }
     );
-  return providers[network];
+  }
+  return providers[url]; // Return provider associated with the URL
 }
 
 export function getBatchedProvider(
-  network,
   { broviderUrl = DEFAULT_BROVIDER_URL }: ProviderOptions = {}
 ) {
-  const url = `${broviderUrl}/${network}`;
-  if (!batchedProviders[network])
-    batchedProviders[network] = new JsonRpcBatchProvider({
+  const url = broviderUrl; // Use the base URL without appending the network (chain ID)
+  if (!batchedProviders[url]) { // Check batched providers by URL instead of network
+    batchedProviders[url] = new JsonRpcBatchProvider({
       url,
       timeout: 25000,
       allowGzip: true
     });
-  return batchedProviders[network];
+  }
+  return batchedProviders[url]; // Return batched provider associated with the URL
 }
